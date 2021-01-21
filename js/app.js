4faf4b2882cf4f9e814a1549ec78bf681c23b389
jtustanic <75272288+jtustanic@users.noreply.github.com>
2021-01-21 20:49:47 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).foundation()
+
++
+$("#B1").click(function(){
+	
+	$("#B1").show();
+	$("#B2").show();
+	$("#B3").show();
+	$("#B4").show();
+	
+});
+
+$("#B2").click(function(){
+	
+	$("#B1").show();
+	$("#B2").show();
+	$("#B3").show();
+	$("#B4").show();
+	
+});
+
+$("#B3").click(function(){
+	
+	$("#B1").show();
+	$("#B2").show();
+	$("#B3").show();
+	$("#B4").show();
+});
+
+$("#B4").click(function(){
+	
+	$("#B1").show();
+	$("#B2").show();
+	$("#B3").show();
+	$("#B4").show();
+	
+});
